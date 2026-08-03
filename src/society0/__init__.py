f1af411e18c79b0f4a6b354d8488bf73e3c7c500
marvin <arcticpfor@yahoo.com>
2026-08-03 21:22:27 +0800

    release: prepare Society0 2.1.0

--- src/society0/__init__.py
+++ src/society0/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from importlib import import_module
 from typing import Any, Dict, Tuple, TYPE_CHECKING
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __all__ = [
     "Society0",
     "LLMModel",
