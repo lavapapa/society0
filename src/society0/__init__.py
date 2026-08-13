1cc45f14addf4c16ef64693eb826010eb5fdde77
marvin <arcticpfor@yahoo.com>
2026-08-14 02:20:51 +0800

    release: Society0 4.1.3 proxy persistence performance

--- src/society0/__init__.py
+++ src/society0/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from importlib import import_module
 from typing import Any, Dict, Tuple, TYPE_CHECKING
 
-__version__ = "4.1.2"
+__version__ = "4.1.3"
 __all__ = [
     "Society0",
     "LLMModel",
